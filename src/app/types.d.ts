e89302c5897af6cee7bdaf2fed87a3a3b5e050ca
Jeremy <encaladajeremy@gmail.com>
2024-01-23 17:58:13 -0500

    Add projects.json and types.d.ts files

---
+++ src/app/types.d.ts
@@ -0,0 +1,121 @@
+type state = 'toDo' | 'inProgress' | 'done'
+type typeOfItem = 'proyect' | 'spring' | 'task' | 'reminder' | 'img' | 'video' | 'audio' | 'link'
+
+export interface Item {
+  i: string
+  x: number
+  y: number
+  w: number
+  h: number
+  minW?: number
+  minH?: number
+  type?: typeOfItem // Añadir type como propiedad común
+}
+
+export interface Proyect {
+  id: number
+  name: string
+  description: string
+  image: string
+  tags: string[]
+  spring?: Spring[]
+}
+
+interface Spring {
+  id: number
+  name: string
+  description: string
+  startDate: Date
+  endDate: Date
+  state: state
+  tasks?: Task[]
+}
+
+interface Task {
+  id: number
+  name: string
+  description: string
+  startDate: Date
+  endDate: Date
+  state: state
+  reminders: Reminder[]
+  board: Board
+  tags: string[]
+}
+
+interface Board {
+  id: number
+  name: string
+  description: string
+  startDate: Date
+  endDate: Date
+  state: state
+  items: Item[]
+}
+
+interface Reminder {
+  id: number
+  name: string
+  description: string
+  startDate: Date
+  endDate: Date
+  state: state
+}
+
+interface Img {
+  id: number
+  name: string
+  description: string
+  url: string
+}
+
+interface Video {
+  id: number
+  name: string
+  description: string
+  url: string
+}
+
+interface Audio {
+  id: number
+  name: string
+  description: string
+  url: string
+}
+
+interface Link {
+  id: number
+  name: string
+  description: string
+  url: string
+}
+
+export type ProjectItem = {
+  type: 'proyect' // Discriminante de tipo
+  data: Proyect
+} & Item
+
+export type SpringItem = {
+  type: 'spring' // Discriminante de tipo
+  data: Spring
+} & Item
+
+export type TaskItem = {
+  type: 'task' // Discriminante de tipo
+  data: Task
+} & Item
+
+export type ReminderItem = {
+  type: 'reminder' // Discriminante de tipo
+  data: Reminder
+} & Item
+
+export type ImgItem = {
+  type: 'img' // Discriminante de tipo
+  data: Img
+} & Item
+
+export type VideoItem = {
+  type: 'video' // Discriminante de tipo
+  data: Video
+} & Item
